32345e5abe06ef5af531e81f057880e03c891b6a
Eugene Tcipnjatov <eugene@freeconferencecall.com>
2021-09-15 21:54:48 -0400

    second commit

--- credentials.py
+++
@@ -1,9 +0,0 @@
-# client_id = "71afecbf56a7a7c2"
-# client_secret = "7abc2293067968e0f0d396a1d9308746f3723309055eb31b"
-# URL = "https://www.freeconferencecall.com/api/v4/"
-
-
-client_id = "87527b4745826f82"
-client_secret = "b2e996e9176a566e0ff79dac7fd094261079ca6d9e131c04"
-URL = "https://www.freeconferencecall.com/api/v4/"
-auth = "74f859fb0eec491a4a92ab14fac610ee"
